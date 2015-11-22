733de1f6165bbec3d99a952ae5fa50276c5bf075
ncastano <nicolasm.castano@gmail.com>
2015-11-22 02:28:48 -0300

    Primer implementación de semaforos

---
+++ Kernel/include/sem.h
@@ -0,0 +1,28 @@
+#ifndef _SEM_H
+#define _SEM_H
+
+#include <stdint.h>
+#include "task.h"
+
+#define NULL (void*)0
+#define MAX_SEM 256
+
+typedef struct{
+	int pids[MAX_TASKS];
+	uint32_t id;
+	uint32_t value;
+	uint32_t currentTask;
+} sem_t;
+
+sem_t *create_sem(uint32_t id, uint32_t value);
+
+void delete_sem(sem_t *sem);
+
+void sem_up(sem_t *sem);
+
+int sem_down(sem_t *sem, int pid);
+
+uint32_t sem_value(sem_t *sem);
+
+sem_t *sem_get(uint32_t id);
+#endif
---
+++ Kernel/sem.c
@@ -0,0 +1,71 @@
+#include "include/pmem.h"
+#include "include/sem.h"
+#include "include/scheduler.h"
+#include "include/define.h"
+
+uint32_t currentId = 0;
+static sem_t *sem_array[MAX_SEM];
+
+sem_t *
+create_sem(uint32_t id, uint32_t value){
+	
+	sem_t *sem = pmem_alloc();
+
+	sem->id = id;
+	sem->value = value;
+	sem->currentTask = 0;
+	
+	if(currentId>MAX_SEM){
+		return NULL;
+	}
+
+	sem_array[currentId++] = sem;
+
+	return sem;
+}
+
+void 
+delete_sem(sem_t *sem){
+	pmem_free(sem);
+}
+
+void 
+sem_up(sem_t *sem){
+	if(sem->currentTask == 0){
+		sem->value++;
+	}
+	resume_task_with_pid(sem->pids[0]);
+	for(int i=0; i<(sem->currentTask);i++){
+		sem->pids[i]=sem->pids[i+1];
+	}
+	sem->currentTask--;
+}
+
+int 
+sem_down(sem_t *sem, int pid){
+	if(sem->value > 0){
+		sem->value--;
+		return TRUE;
+	} else{
+		pause_task_with_pid(pid);
+		sem->pids[sem->currentTask++] = pid;
+	}
+	return FALSE;
+}
+
+uint32_t 
+sem_value(sem_t *sem){
+	return sem->value;
+}
+
+sem_t *
+sem_get(uint32_t id){
+	for(int i =0; i<currentId; i++){
+		if(sem_array[i] != NULL){
+			if(sem_array[i]->id ==id){
+				return sem_array[i];
+			}
+		}
+	}
+	return NULL;
+}
